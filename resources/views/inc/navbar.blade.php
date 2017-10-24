be82ba2b455bb2c5e8f445b49b096353010e5d92
MarijaAngeli <angelimarija@gmail.com>
2017-10-24 13:32:30 +0200

    change 2

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -13,7 +13,7 @@
                     <a class="navbar-brand" href="{{ url('/')}}">
                     ToDoList
                     </a>
-               
+               </div>
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
